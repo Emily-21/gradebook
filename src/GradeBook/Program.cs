5c0765a44446ff146b55f79663dd5977d4af2d9e
Emily Littler <emilylittler1@gmail.com>
2020-06-18 16:31:49 +0100

    setting up fundamentals of cs

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args) //string array= a collection of strings, 0 or more passed in as args... args = arguements, args passed to the app 
+        //this is method named Main, the 'entry point' to the application, start executing this
+        {     
+            List<double> grades = new List<double>() {12.7, 16.2, 12.2, 14.2};
+            grades.Add(16.3);
+            
+
+            double total = 0.0;
+            foreach(double number in grades) 
+            {
+              total += number;
+            }
+            total /= grades.Count;
+            Console.WriteLine($"the avg grade is {total:N1}"); //to 1 decimal place
+            
+        //     if (args.Length > 0)
+        //     {
+        //         Console.WriteLine($"Hello, {args[0]}!"); // here we are saying, check the parameters of the method!
+        //         //which, we are pushing through the command line 'dotnet run Emily'
+        //      }
+        //    else 
+        //     {
+        //         Console.WriteLine("hello");
+        //     }
+         }
+    }
+}
